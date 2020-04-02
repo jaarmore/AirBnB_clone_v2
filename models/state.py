21b4f28ac8222413f9a1114cb7f8b9116625d884
elisa2105 <aikomi1726@gmail.com>
2020-04-02 17:24:54 +0000

    fix state

--- models/state.py
+++ models/state.py
@@ -14,20 +14,19 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
-
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state', cascade="all, delete")
     else:
         name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") == "file":
-        @property
-        def cities(self):
-            list_of_cities = []
-            dic_cities = models.Storage.all(City)
-            for city in dic_cities.items():
-                if city.state_id == self.id:
-                    list_of_cities.append(city)
-            return list_of_cities
+    """if getenv("HBNB_TYPE_STORAGE") == "file":"""
+    @property
+    def cities(self):
+        list_of_cities = []
+        dic_cities = models.Storage.all(City)
+        for city in dic_cities.items():
+            if city.state_id == self.id:
+                list_of_cities.append(city)
+        return list_of_cities
