3f75598e9d507ff0177d0b2ba60e0ff24b3e59ce
jaarmore <jaarmore@gmail.com>
2020-04-02 19:45:20 +0000

    setting to ORM and relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
 
 
 class Amenity(BaseModel):
@@ -8,4 +11,9 @@ class Amenity(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates='amenities')
