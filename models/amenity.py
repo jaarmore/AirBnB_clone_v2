bc847a2667dd8d950984c7ae6c33f253972df897
jaarmore <jaarmore@gmail.com>
2020-04-02 22:40:04 +0000

    Adding ORM settings to file

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String
 from os import getenv
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
