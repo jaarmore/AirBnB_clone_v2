7a793e1cd7c6969173ea4ea81395b84f17dcfe92
elisa2105 <aikomi1726@gmail.com>
2020-04-02 04:00:49 +0000

    fix new method

--- models/base_model.py
+++ models/base_model.py
@@ -28,11 +28,9 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            models.storage.new()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new()
 
     def __str__(self):
         """returns a string
