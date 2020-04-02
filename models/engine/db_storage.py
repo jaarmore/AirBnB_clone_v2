9d12eb2cde183d67b2e8cc80dc1954c15222460d
jaarmore <jaarmore@gmail.com>
2020-04-02 22:14:39 +0000

    adding element Review to classes list

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -33,7 +33,7 @@ class DBStorage():
 
     def all(self, cls=None):
         """ query all objects depending of the class name """
-        classes = ['State', 'City', 'User', 'Place']
+        classes = ['State', 'City', 'User', 'Place', 'Review']
         class_dict = {}
         if cls and cls in classes:
             # busca por el tipo de objeto
