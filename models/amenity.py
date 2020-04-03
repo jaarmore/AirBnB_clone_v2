812e1fc1b0648b1d3bd199b9e5c024203964a618
elisa2105 <aikomi1726@gmail.com>
2020-04-03 02:19:48 +0000

    fix i

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+
+"""
+This module define Amenity class
+"""
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.place import Place, place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
+    """Class Amenity Inherits BaseModel"""
     __tablename__ = "amenities"
+
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity')
+    place_amenities = relationship("Place", secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""This is the place class"""
-import os
+"""
+This module defines the Place Class to AirBnB project.
+"""
+
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float, Table
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+import os
+
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id',
@@ -20,20 +25,7 @@ place_amenity = Table('place_amenity', Base.metadata,
 
 
 class Place(BaseModel, Base):
-    """This is the class for Place
-    Attributes:
-        city_id: city id
-        user_id: user id
-        name: name input
-        description: string of description
-        number_rooms: number of room in int
-        number_bathrooms: number of bathrooms in int
-        max_guest: maximum guest in int
-        price_by_night:: pice for a staying in int
-        latitude: latitude in flaot
-        longitude: longitude in float
-        amenity_ids: list of Amenity ids
-    """
+    """Creates class attributes for Place class"""
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -46,13 +38,11 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
-    reviews = relationship("Review",
-                           backref="place", cascade="all, delete")
-    amenities = relationship("Amenity",
-                             secondary=place_amenity,
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship("Amenity", secondary=place_amenity,
                              viewonly=False)
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
 
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def reviews(self):
             """returns the list of Review instances"""
@@ -76,5 +66,5 @@ class Place(BaseModel, Base):
         @amenities.setter
         def amenities(self, obj):
             """set the ids of the amenities"""
-            if type(obj).__name__ == 'Amenity':
+            if type(obj).__name__ == "Amenity":
                 self.amenity_ids.append(obj.id)
