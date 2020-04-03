fbb513998b6f58f49c36cda18b7fb14f91d29f62
elisa2105 <aikomi1726@gmail.com>
2020-04-03 02:31:23 +0000

    fix files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,71 +1,84 @@
 #!/usr/bin/python3
-""" Storage class for DB for AirBnB Clone_V2 """
-from os import getenv
+
+"""
+Module: db__storage DB storage class
+"""
+
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import scoped_session
-from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 
-class DBStorage():
-    """ Class to connect MySQL """
+class DBStorage:
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        """ Constructor """
-        user = getenv("HBNB_MYSQL_USER")
-        pwd = getenv("HBNB_MYSQL_PWD")
-        host = getenv("HBNB_MYSQL_HOST")
-        dataBase = getenv("HBNB_MYSQL_DB")
-        HBNB_ENV = getenv("HBNB_ENV")
-        dbConnector = 'mysql+mysqldb://{}:{}@{}/{}'
-        self.__engine = create_engine(dbConnector.format(
-            user, pwd, host, dataBase), pool_pre_ping=True)
-        if HBNB_ENV == "test":
+        """Creating engines"""
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                os.getenv("HBNB_MYSQL_USER"),
+                os.getenv("HBNB_MYSQL_PWD"),
+                os.getenv("HBNB_MYSQL_HOST"),
+                os.getenv("HBNB_MYSQL_DB")), pool_pre_ping=True)
+        if os.getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """ query all objects depending of the class name """
-        classes = ['State', 'City', 'User', 'Place', 'Review', 'Amenity']
-        class_dict = {}
-        if cls and cls in classes:
-            # busca por el tipo de objeto
-            _object = self.__session.query(cls).all()
-            for _obj in _object:
-                key = type(_obj).__name__ + '.' + _obj.id
-                class_dict[key] = _obj
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dictionary = {}
+        if cls is None:
+            all_cls = ["State", "City", "User", "Amenity", "Place", "Review"]
+            for cl in all_cls:
+                objs = self.__session.query(eval(cl))
+                for obj in objs:
+                    key = "{}.{}".format(type(obj).__name__, obj.id)
+                    dictionary[key] = obj
         else:
-            for _class in classes:
-                _object = self.__session.query(eval(_class)).all()
-                for _obj in _object:
-                    key = type(_obj).__name__ + '.' + _obj.id
-                    class_dict[key] = _obj
-        return class_dict
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                dictionary[key] = obj
+
+        return dictionary
 
     def new(self, obj):
-        """ add the object to the current database session """
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
         self.__session.add(obj)
 
     def save(self):
-        """ commit all changes of the current database session """
+        """serialize the file path to JSON file path
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """ delete from the current database session obj if not None """
+        """to delete obj from __objects if its inside
+        """
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """Reload objects """
+        """Reload objs"""
         Base.metadata.create_all(self.__engine)
-        Session = scoped_session(sessionmaker(
-            bind=self.__engine, expire_on_commit=False))
+        Session_sc = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(Session_sc)
         self.__session = Session()
