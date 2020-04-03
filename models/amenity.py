f0669ba91deb51b2c01211ae075f75f427514156
jaarmore <jaarmore@gmail.com>
2020-04-03 00:30:55 +0000

    fix code on file

--- models/amenity.py
+++ models/amenity.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from os import getenv
 
 
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
         name: input name
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = relationship("Place", secondary="place_amenity",
-                                       back_populates='amenities')
+    place_amenities = relationship("Place", secondary="place_amenity")
