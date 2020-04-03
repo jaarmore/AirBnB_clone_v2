d5adb88f98b9f52884e7414720e056abca46e1ee
jaarmore <jaarmore@gmail.com>
2020-04-03 00:40:21 +0000

    fix attribute secondary on file

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This is the amenity class"""
 from models.base_model import BaseModel, Base
+from models.place import Place, place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place, place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -13,4 +13,4 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity")
+    place_amenities = relationship("Place", secondary=place_amenity)
