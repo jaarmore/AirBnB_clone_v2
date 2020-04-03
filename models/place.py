f9345e076a87b9fbb3d4b9a85a6369f2f375012e
jaarmore <jaarmore@gmail.com>
2020-04-03 00:16:27 +0000

    adding setting to db in file

--- models/place.py
+++ models/place.py
@@ -10,15 +10,17 @@ from sqlalchemy import Table
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True,
+                                 nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True,
+                                 nullable=False))
 
 
 class Place(BaseModel, Base):
