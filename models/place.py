b3cd2110f69772b3a923f1b8899ac031f03c6744
jaarmore <jaarmore@gmail.com>
2020-04-03 00:45:45 +0000

    changin name of relationship to plae_amenity

--- models/place.py
+++ models/place.py
@@ -7,15 +7,15 @@ from sqlalchemy.orm import relationship
 from models.amenity import Amenity
 from models.review import Review
 from os import getenv
-association_table = Table("place_amenity", Base.metadata,
-                          Column("place_id", String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True,
-                                 nullable=False))
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -45,7 +45,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="all, delete")
-    amenities = relationship("Amenity", secondary="place_amenity")
+    amenities = relationship("Amenity", secondary=place_amenity)
     amenity_ids = []
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
